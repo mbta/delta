74ba2764d10c5f96ceb2ebd91b29825a5c61ca49
Paul Swartz <paul@paulswartz.net>
2020-04-01 13:07:09 -0400

    fix(Producer): log the full error message

--- lib/delta/producer.ex
+++ lib/delta/producer.ex
@@ -107,7 +107,9 @@ defmodule Delta.Producer do
   def handle_error(state, reason) do
     _ =
       Logger.warn(fn ->
-        "#{__MODULE__} error fetching url=#{inspect(state.conn.url)} error=#{inspect(reason)}"
+        "#{__MODULE__} error fetching url=#{inspect(state.conn.url)} error=#{
+          inspect(reason, limit: :infinity)
+        }"
       end)
 
     {:noreply, [], state}
